15cf60b9baf91f861ccfd1e173c2e5ad166bb223
lu4ezar <misha-shisha@mail.ru>
2020-05-15 17:15:22 +0700

    set header colors

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#2e3f7f`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#f2f8fd`,
             textDecoration: `none`,
           }}
         >
