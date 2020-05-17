f63a27a2e6010bdd2a1fb364971f7ce4290e6adf
lu4ezar <misha-shisha@mail.ru>
2020-05-18 01:19:52 +0700

    change header bg color

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#2e3f7f`,
+      background: `#3b3c40`,
       marginBottom: `1.45rem`,
     }}
   >
