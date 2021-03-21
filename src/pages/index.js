b1a641aa01097a9e21aaf30041fedc8c21f8f2f3
lu4ezar <misha-shisha@mail.ru>
2021-03-21 22:09:21 +0700

    add framer stuff to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,29 +3,42 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Project from "../components/project"
 import { usePinnedReposQuery } from "../query"
+import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion"
+import styles from "../styles/project.module.css"
+import Modal from "../components/modal"
 
 const IndexPage = () => {
   const pinnedRepos = usePinnedReposQuery()
-  const [selectedCard, setSelectedCard] = React.useState(null)
+  const [selectedProjectId, setSelectedProjectId] = React.useState(null)
+  const selectedProject = pinnedRepos.find(
+    repo => repo.id === selectedProjectId
+  )
   return (
     <Layout>
       <SEO title="Home" />
-      <div
-        style={{
-          display: `flex`,
-          flexWrap: `wrap`,
-          justifyContent: `space-around`
-        }}
-      >
-        {pinnedRepos.map(repo => (
-          <Project
-            key={repo.id}
-            project={repo}
-            selected={repo.id === selectedCard}
-            select={() => setSelectedCard(repo.id)}
-          />
-        ))}
-      </div>
+      <AnimateSharedLayout type="crossfade">
+        <motion.div className={styles.container} key="page">
+          {pinnedRepos.map(repo => (
+            <Project
+              key={repo.id}
+              selected={selectedProjectId === repo.id}
+              project={repo}
+              onClick={() => setSelectedProjectId(repo.id)}
+            />
+          ))}
+          <AnimatePresence>
+            {selectedProjectId && (
+              <Modal key="modal">
+                <Project
+                  selected
+                  project={selectedProject}
+                  onClick={() => setSelectedProjectId(null)}
+                />
+              </Modal>
+            )}
+          </AnimatePresence>
+        </motion.div>
+      </AnimateSharedLayout>
     </Layout>
   )
 }
